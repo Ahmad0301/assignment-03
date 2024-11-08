7a81ea28162940283354023f44b7098312fe8f9b
Ahmad0301 <sehroz3040@gmail.com>
2024-11-08 23:22:41 +0500

    creat delet api

--- index.js
+++ index.js
@@ -33,6 +33,35 @@ app.get('/read',(req,resp)=>{
         })
     })
 
+    app.delete('/delet', (req, resp) => {
+        
+        fs.unlink(filePath, (error) => {
+            if (error) {
+                console.log(error);
+                resp.status(500).send('Error deleting the file');
+            } else {
+                resp.status(200).send('File deleted successfully');
+            }
+        });
+    });
+
+
+    app.put('/put', (req, resp) => {
+        const updatedBook = req.body;
+    
+        fs.writeFile('new.json', JSON.stringify(updatedBook), (error) => {
+            if (error) {
+                console.log(error);
+                resp.status(500).send('Error updating the file');
+            } else {
+                resp.status(200).send('File updated successfully');
+            }
+        });
+    });
+    
+    
+
 app.listen(3000,(req,resp)=>{
     console.log('Server is running on port 3000')
 })
+
